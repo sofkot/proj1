cd5cab538ab092469cf2edb5d728568a6fe55722
sofkot <39833677+sofkot@users.noreply.github.com>
2018-06-15 19:33:02 +0300

    Update and rename 1 to script.js

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+function start() {
+   document.getElementById('q1').style.display = "block";
+}
+function showQ2() {
+   document.getElementById('q2').style.display = "block";
+}
+function cancel() {
+   document.getElementById('q1').style.display = "none";
+   document.getElementById('q2').style.display = "none";
+   document.getElementById('q3').style.display = "none";
+   document.getElementById('q4').style.display = "none";
+}
+function showQ3() {
+   document.getElementById('q3').style.display = "block";
+}
+function showQ4() {
+   document.getElementById('q4').style.display = "block";
+}
+
